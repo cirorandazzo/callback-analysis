febaa7ed710c676ebf2199eb801af706d7a7ddc5
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-08-20 15:37:18 -0700

    fix frame alignment thresholding

--- utils/video.py
+++ utils/video.py
@@ -5,16 +5,35 @@
 #
 
 
-def get_video_frames_from_callback_audio(camera_channel_audio):
+def get_video_frames_from_callback_audio(
+    camera_channel_audio: np.ndarray,
+    threshold_function=lambda x: np.max(x) * 0.2,
+    do_check=True,
+) -> np.ndarray:
     """
-    takes ONE CHANNEL of audio as a numpy array, and returns indices when that crosses mean value (threshold)
+    - takes ONE CHANNEL of audio as a numpy array
+    - return all frames when audio starts dips below threshold (ie, frame i for i<=thresh iff (i-1)>thresh)
+    - if do_check is True, ensures that range of inter-frame intervals <= 10
     """
     import numpy as np
 
-    threshold = np.mean(camera_channel_audio)
+    threshold = threshold_function(camera_channel_audio)
 
-    a_thresholded = camera_channel_audio > threshold  # to bool
-    offset = np.append([0], a_thresholded[:-1])
+    a_thresholded = camera_channel_audio <= threshold  # to bool
 
-    # return a_thresholded & ~offset
-    return np.nonzero(a_thresholded & ~offset)[0]
+    # one frame prior. don't allow first frame
+    offset = np.append([1], a_thresholded[:-1])
+
+    frames = np.nonzero(a_thresholded & ~offset)[0]
+
+    if do_check:
+        allowable_range = 10
+
+        # number of audio samples between subsequent frames
+        deltas = frames[1:] - frames[:-1]
+
+        assert (
+            np.ptp(deltas) <= allowable_range
+        ), "Warning! Frame timings might vary too much. You might need a stricter threshold function."
+
+    return frames
