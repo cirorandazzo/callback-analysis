909d73785a58f0314b6c3f3ed0ba3a930e61e59a
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2024-12-16 15:49:08 -0800

    breaths: refine metadata

--- breaths-make_df-plot.py
+++ breaths-make_df-plot.py
@@ -5,7 +5,7 @@ import os
 
 import numpy as np
 import pandas as pd
-from scipy.signal import butter
+from scipy.signal import butter, filtfilt
 
 import matplotlib.pyplot as plt
 
@@ -26,17 +26,19 @@ from utils.video import get_triggers_from_audio
 # %%
 # get filelist
 
-# path = r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd99rd72/preLesion/callbacks/rand/230215/*-B*.wav"
-# path = r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/pk19br8/preLesion/callback/rand/**/*-B*.wav"
-# path = r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd56/preLesion/callbacks/male_230117/*-B*.wav"
-# path = r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd57rd97/preLesion/callbacks/male_230117/*-B*.wav"
-path = r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd99rd72/preLesion/callbacks/rand/230215/*-B*.wav"
+paths = [
+    r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd99rd72/preLesion/callbacks/rand/230215/*-B*.wav",
+    r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/pk19br8/preLesion/callback/rand/**/*-B*.wav",
+    r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd56/preLesion/callbacks/male_230117/*-B*.wav",
+    r"M:/eszter/behavior/air sac calls/HVC lesion/aspiration/rd57rd97/preLesion/callbacks/male_230117/*-B*.wav",
+]
+
+default_bird = "rd56"  # only bird which fails name parsing
 
 # `*-B*` excludes "-PostBlock"
 
-path = os.path.normpath(path)
-
-files = sorted(glob.glob(path))
+# get all files matching above paths
+files = [file for path in paths for file in sorted(glob.glob(os.path.normpath(path)))]
 
 assert len(files) != 0, "No files found!"
 
@@ -48,13 +50,17 @@ for i, f in enumerate(files):
 # construct trial-by-trial df across all files
 all_trials = pd.DataFrame()
 
+fs = 44100
+b, a = butter(N=2, Wn=50, btype="low", fs=fs)
+
 for f in files:
 
     # load audio
     channels = AudioObject.from_wav(
         f, channels="all", channel_names=["audio", "breathing", "trigger"]
     )
-    fs = channels[1].fs
+
+    assert fs == channels[1].fs
     breath = channels[1].audio
 
     # threshold stimuli; assume 100ms length
@@ -62,8 +68,8 @@ for f in files:
 
     # segment breaths based on smoothed waveform
     centering = lambda x: np.percentile(x, 45)
-    b, a = butter(N=2, Wn=50, btype="low", fs=channels[1].fs)
     exps, insps = segment_breaths(breath, channels[1].fs, threshold=centering, b=b, a=a)
+    breath_zero_point = centering(filtfilt(b, a, breath))
 
     # get onsets, offsets, labels
     onsets, offsets, labels = make_notmat_vars(
@@ -96,6 +102,7 @@ for f in files:
     )
 
     stim_trials["wav_filename"] = f
+    stim_trials["breath_zero_point"] = breath_zero_point
 
     # putative stim phase: for now just "exp" or "insp", based on first "call"
     stim_trials["stim_phase"] = stim_trials["calls_in_range"].apply(
@@ -121,6 +128,9 @@ all_trials
 # %%
 # plot & make json records
 
+exist_ok = True  # False --> error out if folder already exists
+skip_replot = True  # True --> if the plot path already exists, skip replot (just process metadata)
+
 pre_time_s = 0.1
 post_time_s = 3.1
 ylims = [-3500, 10000]
@@ -133,20 +143,31 @@ for file in np.unique(all_trials.index.get_level_values("wav_filename")):
     basename = os.path.split(root)[-1]
 
     # metadata
-    bird = parse_birdname(root)
+    try:
+        bird = parse_birdname(root)
+        # bird = "rd56"
+    except TypeError:
+        if default_bird is not None:
+            bird = default_bird
+        else:
+            raise TypeError(
+                f"Couldn't parse birdname from: {root}\nIf this bird just has one band, you should find this and hard-code its name."
+            )
+
     block = int(parse_parameter_from_string(root, "Block", chars_to_ignore=0))
 
     # load audio & relevant stims
     stim_trials = all_trials.xs(file)
     ao = AudioObject.from_wav(file, channels=1)
-    m = centering(ao.audio)
 
     audiofile_plot_folder = os.path.join(figure_root_dir, bird, basename)
-    os.makedirs(audiofile_plot_folder)
+    os.makedirs(audiofile_plot_folder, exist_ok=exist_ok)
 
     # for each stim trial in this audio file
     for t in stim_trials.index:
         # make metadata for json
+        plot_filename = os.path.normpath(os.path.join(audiofile_plot_folder, f"tr{t}.jpg"))
+
         metadata = {
             k: stim_trials.loc[t, k]
             for k in [
@@ -155,16 +176,16 @@ for file in np.unique(all_trials.index.get_level_values("wav_filename")):
                 "calls_index",
                 "stim_phase",
                 "putative_call",
+                "breath_zero_point",
             ]
         }
         metadata["wav_filename"] = file
+        metadata["trial"] = t
         metadata["bird"] = bird
         metadata["block"] = block
         metadata["call_types"] = np.unique(stim_trials.loc[t, "call_types"])
         metadata["plot_id"] = f"{basename}-tr{t}"
-        metadata["plot_filename"] = os.path.normpath(
-            os.path.join(audiofile_plot_folder, f"tr{t}.jpg")
-        )
+        metadata["plot_filename"] = plot_filename
 
         st = metadata["trial_start_s"]
         en = metadata["trial_end_s"]
@@ -172,24 +193,28 @@ for file in np.unique(all_trials.index.get_level_values("wav_filename")):
         if np.isinf(metadata["trial_end_s"]):
             metadata["trial_end_s"] = "Inf"
 
+        breath_filt = filtfilt(b, a, ao.audio)
+
         fig, ax = plt.subplots(figsize=(10, 5))
-        ax = plot_breath_callback_trial(
-            ao.audio,
-            ao.fs,
-            stim_trials.loc[t],
-            m,
-            pre_time_s,
-            post_time_s,
-            ylims,
-            st,
-            en,
-            ax=ax,
-            color_dict={"exp": "r", "insp": "b"},
-        )
 
-        ax.set(title=metadata["plot_id"])
+        if not os.path.exists(plot_filename) or not skip_replot:
+            ax = plot_breath_callback_trial(
+                audio=breath_filt,
+                fs=ao.fs,
+                stim_trial=stim_trials.loc[t],
+                y_breath_labels=metadata["breath_zero_point"],
+                pre_time_s=pre_time_s,
+                post_time_s=post_time_s,
+                ylims=ylims,
+                st=st,
+                en=en,
+                ax=ax,
+                color_dict={"exp": "r", "insp": "b"},
+            )
 
-        fig.savefig(metadata["plot_filename"])
+            ax.set(title=metadata["plot_id"])
+
+            fig.savefig(plot_filename)
         records[metadata["plot_id"]] = metadata
         plt.close()
 
@@ -219,6 +244,8 @@ json_filename = os.path.join(figure_root_dir, "plot_metadata.json")
 if os.path.exists(json_filename):
     with open(json_filename, "r") as jf:
         extant_records = {x["plot_id"]: x for x in json.loads(jf.read())}
+else:
+    extant_records = {}
 
 # adds new records, overwriting extant records with same plot id
 for k, v in records.items():
@@ -226,4 +253,10 @@ for k, v in records.items():
 
 # write records
 with open(json_filename, "w") as f:
-    json.dump(list(extant_records.values()), f, indent=4, cls=NumpyEncoder)
+    # sorts by key before dumping
+    json.dump(
+        [extant_records[k] for k in sorted(extant_records.keys())],
+        f,
+        indent=4,
+        cls=NumpyEncoder,
+    )
