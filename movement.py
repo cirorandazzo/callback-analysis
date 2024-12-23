a45dc52b81cb4a99d420ea45ddabc84771599d79
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-12-23 15:35:39 -0500

    exploration of movement

--- movement.py
+++ movement.py
@@ -136,24 +136,84 @@ def plot_latency_by_trial(
 
     return ax
 
+def plot_timeseries_subplots(data, xs, titles, fs, tf=lambda x: np.median(x) * 1.2, in_s=True,):
+    fig, axs = plt.subplots(nrows=len(data), sharex=True)
+
+    markers = dict(
+        zorder=3,
+        marker="+",
+    )
+
+    for ax, x, y, t in zip(axs.ravel(), xs, data, titles):
+        if x is None:
+            x = np.arange(len(y))
+
+        if t in ["loom_trigs"]:
+            if in_s:
+                loom_x = loom_onsets / fs
+            else:
+                loom_x = loom_onsets
+
+            ax.scatter(loom_x, y[loom_onsets], c="r", **markers)
+
+        if t in ["avg_change", "max_change"]:
+            crossings = get_triggers_from_audio(
+                y,
+                threshold_function=tf,
+                crossing_direction="up",
+            )
+
+            x_cr = x[crossings]
+            y_cr = y[crossings]
+
+            if in_s:
+                x_cr = x_cr / fs
+
+            ax.scatter(x_cr, y_cr, c="green", **markers)
+
+        # plot actual timeseries
+        if in_s:
+            x = x / fs  # convert to seconds
+
+        ax.plot(x, y)
+
+        # testing thresholds
+        thr = tf(y)
+        ax.plot([x.min(), x.max()], [thr, thr], c="k", linestyle="dotted")
+
+        ax.set(
+            title=t,
+        )
+
+    axs[-1].set(
+        xlabel="Time (s)",
+        # xlim=(1, 2),
+        # xlim=(31.4, 32),
+    )
+
+    fig.tight_layout()
+
+    return fig, axs
+
 
 if __name__ == "__main__":
     verbose = True
     do_plot = True
+    suppress_remake_diff = True
+    plt.rcParams['figure.dpi'] = 400
 
-    birdname = "or54rd45"
-    # birdname = "or14pu27"
+    # birdname = "or54rd45"
+    birdname = "or14pu27"
     pattern = f"/Volumes/users/randazzo/callbacks/loom_only/{birdname}/loom_only/*_CAM0-0000.avi"
     cam_n = 1
     diff_video_folder = "/Volumes/PlasticBag/anxiety_calls/loom_only-diff_videos"
-
     fs = 44100
 
     ignore_range_fr = round(fs * 0.2)
 
     movement_threshold_function = lambda x: np.median(x) * 1.2
-    suppress_remake_diff = True
-    window_length = round(300 * (fs / 1000))  # 300 ms --> fr
+    
+    window_length = round(500 * (fs / 1000))  # 300 ms --> fr
 
     os.makedirs(diff_video_folder, exist_ok=True)
 
@@ -202,7 +262,7 @@ if __name__ == "__main__":
                 print(f"Wrote diff video: {diff_video_fname}")
 
         elif verbose:
-            print("suppress_remake_diff is on & diff video already exists! Skipping...")
+            print("suppress_remake_diff is on & diff video already exists! Skipping to analysis...")
 
         # %% track movement
         max_change, avg_change = get_max_avg_pixel_changes(diff_video_fname)
@@ -222,14 +282,32 @@ if __name__ == "__main__":
         movement_ii = vid_frames_ii[thresholded_movement_ii_vid]
 
         # %% get movement information for each loom trigger
+        block = rootname.split("Block")[1]
+
         this_df = get_loom_latency_df(loom_onsets, window_length, movement_ii, fs)
         this_df.index.name = "loom"
         this_df["birdname"] = birdname
         this_df["filename"] = avi_path
-        this_df["block"] = rootname.split("Block")[1]
+        this_df["block"] = block
         this_df.set_index(["birdname", "block"], append=True, inplace=True)
 
         all_data.append(this_df)
+
+        if do_plot:
+            data = [bird_audio.audio_frs, loom_trigs, avg_change]
+            xs = [None, None, vid_frames_ii]
+            titles = ["audio", "loom_trigs", "avg_change"]
+
+            fig, ax = plot_timeseries_subplots(data, xs, titles, fs, tf=movement_threshold_function, in_s=True)
+            fig.suptitle(f"{birdname} Block {block}", y=1, fontweight="bold")
+
+            folder = os.path.join(diff_video_folder, birdname)
+            os.makedirs(folder, exist_ok=True)
+            plt.savefig(
+                os.path.join(folder, f"{birdname}-timeseries-block{block}.png"),
+            )
+
+            plt.close(fig)
 
     # %% concat & save
     all_data = pd.concat(all_data)
@@ -243,13 +321,13 @@ if __name__ == "__main__":
 
         plt.savefig(
             os.path.join(diff_video_folder, f"mvmt_latency-{birdname}.png"),
-            dpi=400
         )
+        plt.close()
 
     with open(
         os.path.join(diff_video_folder, f"all_data-{birdname}.pickle"), "wb"
-    ) as f:
-        pickle.dump(all_data, f)
+    ) as plot_timeseries_subplots:
+        pickle.dump(all_data, plot_timeseries_subplots)
 
     if verbose:
         print(f"Finished! Saved dataframe to:\t{diff_video_folder}")
